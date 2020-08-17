2f6c0614f8dd9a9ee0726ac566cfbeaff8f7071d
Nageshwar Verma <nageshv07@gmail.com>
2020-08-17 21:25:46 +0530

    Updates

---
+++ precache-manifest.8eb5002b37571359d843854ec134eb3a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d931be9589300aeb722bddb06c2fbeb",
+    "url": "/revents/index.html"
+  },
+  {
+    "revision": "47ede204180088d5bd42",
+    "url": "/revents/static/css/2.b56a0318.chunk.css"
+  },
+  {
+    "revision": "217f3f1304ca6f89dc1f",
+    "url": "/revents/static/css/main.1c5bc632.chunk.css"
+  },
+  {
+    "revision": "47ede204180088d5bd42",
+    "url": "/revents/static/js/2.9db6ff82.chunk.js"
+  },
+  {
+    "revision": "a52153bcc847b73a84a3dde72b2b9c2e",
+    "url": "/revents/static/js/2.9db6ff82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "217f3f1304ca6f89dc1f",
+    "url": "/revents/static/js/main.0837fdd6.chunk.js"
+  },
+  {
+    "revision": "ca18d68d77edc172cafb",
+    "url": "/revents/static/js/runtime-main.a713153e.js"
+  }
+]);
--- src/features/sandbox/testModal.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ModalWrapper from '../../app/common/modals/modalWrapper'
-export default function TestModal({data}){
-    return(
-        <ModalWrapper size="sm" header="Test Header">
-            <div>The data is : {data}</div>
-        </ModalWrapper>
-    )
-}
